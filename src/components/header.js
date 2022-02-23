f22db61b1ec8fea9c5b670b542a3c6c9f4679ae9
mengchaoli <lmcwesdxc@163.com>
2022-02-22 22:27:26 -0800

    update

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className='header'>
+      <ul className='list'>
+        <li>ABOUT</li>
+        <li>PROJECTS</li>
+        <li>CONTACT</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
