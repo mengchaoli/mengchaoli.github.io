13d560f8557093601e3db892b5eded2cd9871ee9
mengchaoli <lmcwesdxc@163.com>
2022-02-24 22:28:04 -0800

    understand transition

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,20 @@
-function Header() {
+import React, { useState } from 'react';
+import Hamburger from 'hamburger-react';
+
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className='header'>
-      <ul className='list'>
-        <li>ABOUT</li>
-        <li>PROJECTS</li>
-        <li>CONTACT</li>
-      </ul>
+      <Hamburger className='hamburger-btn' toggled={isOpen} toggle={setOpen} />
+      <div className={`nav ${isOpen ? 'open' : ''}`}>
+        <ul className='nav-list'>
+          <li>ABOUT</li>
+          <li>PROJECTS</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Header;
