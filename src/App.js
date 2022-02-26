be20de5bc2bbbe2beb3d8ab6116ec308f9bae513
mengchaoli <lmcwesdxc@163.com>
2022-02-25 17:28:35 -0800

    finish mobile hamburger and nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from './components/header';
 import Footer from './components/footer';
+import HomePage from './components/content/home';
+import AboutPage from './components/content/about';
+import ProjectPage from './components/content/projects';
 
 import './static/css/main.scss'; // all of our styles
 
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />}></Route>
+          <Route path='/projects' element={<ProjectPage />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import Hamburger from 'hamburger-react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const navListArray = ['/about', '/projects', '/contact'];
   return (
     <div className='header'>
-      <Hamburger className='hamburger-btn' toggled={isOpen} toggle={setOpen} />
+      <Hamburger
+        toggled={isOpen}
+        toggle={setOpen}
+        color={isOpen ? '#0bd3d3' : '#f890e7'}
+      />
       <div className={`nav ${isOpen ? 'open' : ''}`}>
         <ul className='nav-list'>
-          <li>ABOUT</li>
-          <li>PROJECTS</li>
-          <li>CONTACT</li>
+          {navListArray.map((navListItem) => {
+            return (
+              <li className='nav-list__item'>
+                <NavLink
+                  to={navListItem}
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                  className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                  {navListItem}
+                </NavLink>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
