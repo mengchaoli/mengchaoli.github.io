3bfed59a74882c93c8b22aa0f197a5aa52e1adb4
mengchaoli <lmcwesdxc@163.com>
2022-03-01 02:02:19 -0800

    rename scss layout folder to components, finish project page basic layout

---
+++ src/components/project-card.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ProjectCard = (props) => {
+  const { detailLink } = props;
+  return <div className='p-card'>Test</div>;
+};
+
+export default ProjectCard;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,19 @@
+import React, { Fragment } from 'react';
+import ProjectCard from '../components/project-card';
+const projectList = [
+  { detailLink: '123', coverImg: '123' },
+  { detailLink: '123', coverImg: '123' },
+  { detailLink: '123', coverImg: '123' },
+];
 const ProjectPage = () => {
-  return <h1>ProjectPage</h1>;
+  // we can put this in a config file later
+  return (
+    <div className='project-wrapper'>
+      {projectList.map((project) => (
+        <ProjectCard />
+      ))}
+    </div>
+  );
 };
 
 export default ProjectPage;
