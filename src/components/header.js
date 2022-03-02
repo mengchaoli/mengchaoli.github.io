c62c018d1b8f632c5ae617153f801bcf15ee9bef
mengchaoli <lmcwesdxc@163.com>
2022-03-02 00:23:22 -0800

    make nav work on desktop, responsive

--- src/components/header.js
+++ src/components/header.js
@@ -13,26 +13,7 @@ const Header = () => {
         color={isOpen ? '#0bd3d3' : '#f890e7'}
       />
       <div className={`nav ${isOpen ? 'open' : ''}`}>
-        <ul className='nav-list'>
-          {navListArray.map((navListItem) => {
-            return (
-              <li className='nav-list__item'>
-                <NavLink
-                  to={navListItem}
-                  onClick={() => {
-                    setOpen(false);
-                  }}
-                  className={({ isActive }) => (isActive ? 'active' : '')}
-                >
-                  {navListItem}
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className='nav-bar'>
-        <ul className='nav-list'>
+        <ul className={`nav-list ${isOpen ? 'open' : ''}`}>
           {navListArray.map((navListItem) => {
             return (
               <li className='nav-list__item'>
