754ec1f40032aa83f1883ff7cab857c57764b624
mengchaoli <lmcwesdxc@163.com>
2022-03-01 23:01:37 -0800

    wip

--- src/components/header.js
+++ src/components/header.js
@@ -31,6 +31,25 @@ const Header = () => {
           })}
         </ul>
       </div>
+      <div className='nav-bar'>
+        <ul className='nav-list'>
+          {navListArray.map((navListItem) => {
+            return (
+              <li className='nav-list__item'>
+                <NavLink
+                  to={navListItem}
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                  className={({ isActive }) => (isActive ? 'active' : '')}
+                >
+                  {navListItem}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
